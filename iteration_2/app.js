f24db100f8748f9de566e93d9326008a2a88b941
Gonzalo Santos <gsanore@gmail.com>
2022-03-07 16:40:20 +0100

    add iteration_2

--- iteration_2/app.js
+++ iteration_2/app.js
@@ -1,5 +1,17 @@
 //2.1 Convierte la siguiente promesa para esperar a ejecutar el console usando async-await.
 
+/* const runTimeOut = () => {
+    const promise = new Promise((resolve) => {
+        setTimeout(function () {
+            resolve();
+        }, 2000);
+    })
+
+    promise.then(() => {console.log('Time out!')})
+};
+
+runTimeOut();*/
+
 const runTimeOut = async () => {
   await new Promise((resolve) => {
     setTimeout(function () {
@@ -13,6 +25,12 @@ const runTimeOut = async () => {
 runTimeOut();
 
 // 2.2 Convierte la siguiente función con un fetch utilizando async-await. Recuerda que para usar .fetch() tendrás que probar el ejercicio en el navegador.
+
+/* function getCharacters () {
+    fetch('https://rickandmortyapi.com/api/character').then(res => res.json()).then(characters => console.log(characters));
+}
+
+getCharacters(); */
 
 async function getCharacters() {
   let res = await fetch('https://rickandmortyapi.com/api/character');
